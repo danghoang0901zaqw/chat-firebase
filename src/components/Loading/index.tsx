8652f85029c80e2f50fe0c054b4c817201390afd
Hoang Tran <hoang.tran@apps-cyclone.com>
2024-03-08 13:58:17 +0700

    feat(loading):build loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='w-6 h-6 rounded-full border-[3px] border-gray-500 border-t-[3px] border-t-gray-200 animate-spin'></div>
+  )
+}
+
+export default Loading
