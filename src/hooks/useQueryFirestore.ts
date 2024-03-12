8445e1938d9362e9277470470ff1411f228f1091
Hoang Tran <hoang.tran@apps-cyclone.com>
2024-03-12 15:40:14 +0700

    feat(query-firestore):add order by and limit

--- src/hooks/useQueryFirestore.ts
+++ src/hooks/useQueryFirestore.ts
@@ -1,41 +1,43 @@
 import { db } from '@/firebase/config';
 import { Condition } from '@/types/chat';
-import { collection, onSnapshot, orderBy, query, where } from 'firebase/firestore';
+import { collection, limit, onSnapshot, orderBy, query, where } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
-import { useAuth } from './useAuth';
 
-const useQueryFirestore = (collectionName: string, condition: Condition) => {
-  const [document, setDocument] = useState([]);
-  const user = useAuth();
+const useQueryFirestore = (collectionName: string, condition: Condition, limited: number = 20) => {
+    const [document, setDocument] = useState([]);
 
-  useEffect(() => {
-    const collectionRef = collection(db, collectionName);
-    let q
-    if (!condition) return
-    if (!condition.operator || !condition.value) return;
-    q = query(collectionRef, where(condition.fieldName, condition.operator, condition.value)), orderBy('createAt');
+    useEffect(() => {
+        const collectionRef = collection(db, collectionName);
+        let q;
+        if (!condition) return;
+        if (!condition.operator || !condition.value) return;
+        q = query(
+            collectionRef,
+            where(condition.fieldName, condition.operator, condition.value),
+            orderBy('createdAt', 'desc'),
+            limit(limited),
+        );
+        const unsubcribe = onSnapshot(
+            q,
+            (snapshot) => {
+                const documents: any = snapshot.docs.map((doc) => {
+                    let data = doc.data();
+                    let docId = doc.id;
+                    return {
+                        ...data,
+                        id: docId,
+                    };
+                });
+                setDocument(documents);
+            },
+            (error) => {
+                console.log(error);
+            },
+        );
 
-    const unsubcribe = onSnapshot(
-      q,
-      (snapshot) => {
-        const documents: any = snapshot.docs.map((doc) => {
-          let data = doc.data();
-          let docId = doc.id;
-          return {
-            ...data,
-            id: docId,
-          };
-        });
-        setDocument(documents);
-      },
-      (error) => {
-        console.log(error);
-      },
-    );
+        return () => unsubcribe();
+    }, [collectionName, condition, limited]);
 
-    return () => unsubcribe();
-  }, [collectionName, condition]);
-
-  return document;
+    return document;
 };
 export default useQueryFirestore;
