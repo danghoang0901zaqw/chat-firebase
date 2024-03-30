57e01b957f45501d82ef402e4805ab34230e8b18
Hoang Tran <hoang.tran@apps-cyclone.com>
2024-03-30 22:13:49 +0700

    feat(loading):add loading when refresh page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
 'use client';
+import Loading from '@/components/Loading';
+import { useAuth } from '@/hooks/useAuth';
 import ChatWindow from '@/views/home/ChatWindow';
 import RoomOptions from '@/views/home/RoomOptions';
 import Sidebar from '@/views/home/Sidebar';
 
 export default function Home() {
-  return (
-    <div className='h-screen flex w-full'>
-      <Sidebar />
-      <ChatWindow />
-      <RoomOptions/>
-    </div>
-  );
+    const user = useAuth();
+    if (!user)
+        return (
+            <div className="h-screen flex items-center justify-center">
+                <Loading />
+            </div>
+        );
+    return (
+        <div className="h-screen flex w-full">
+            <Sidebar />
+            <ChatWindow />
+            <RoomOptions />
+        </div>
+    );
 }
